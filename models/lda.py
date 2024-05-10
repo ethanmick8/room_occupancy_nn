473f9a914107030320bd05ee2f101ca03c5ca9a9
hexa <ethanmick741@gmail.com>
2024-05-10 01:29:33 -0500

    Added comments for SVM and LDA

--- models/lda.py
+++ models/lda.py
@@ -2,15 +2,48 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.metrics import accuracy_score
 
 class EJM_LDA:
+    """
+    This class wraps the sklearn LinearDiscriminantAnalysis model to provide a more straightforward
+    interface for training and predicting labels based on linear discriminant analysis.
+    """
     def __init__(self):
+        """
+        Initializes the Linear Discriminant Analysis (LDA) model.
+        """
         self.model = LinearDiscriminantAnalysis()
 
     def train(self, X_train, y_train):
+        """
+        Trains the LDA model using the provided training data.
+
+        Args:
+            X_train (array-like): Feature dataset for training.
+            y_train (array-like): Labels corresponding to X_train.
+        """
         self.model.fit(X_train, y_train)
 
     def predict(self, X):
+        """
+        Predicts the labels for the given data using the trained LDA model.
+
+        Args:
+            X (array-like): Feature dataset for which labels need to be predicted.
+
+        Returns:
+            array: Predicted labels for the input data.
+        """
         return self.model.predict(X)
 
     def evaluate(self, X, y):
+        """
+        Evaluates the accuracy of the LDA model on the provided dataset.
+
+        Args:
+            X (array-like): Feature dataset for evaluation.
+            y (array-like): True labels for X.
+
+        Returns:
+            float: The accuracy of the model on the provided data.
+        """
         predictions = self.predict(X)
         return accuracy_score(y, predictions)
--- models/svm.py
+++ models/svm.py
@@ -3,16 +3,30 @@ from sklearn.metrics import accuracy_score
 from sklearn.decomposition import PCA
 from sklearn.pipeline import Pipeline
 class EJM_SVM:
+    """
+    This class encapsulates an SVM model allowing for both simple SVM
+    and primarly the ease of variations in the kernel and C. Scikit-learn
+    is utilized to streamline implementation.
+    """
     def __init__(self, C=1.0, kernel='rbf'):
         self.model = SVC(C=C, kernel=kernel)
 
     def train(self, X_train, y_train):
+        """_summary_ This method trains the SVM model using the provided training data.
+
+        Args:
+            X_train (_type_): _description_ The input features
+            y_train (_type_): _description_ The target values
+        """
         self.model.fit(X_train, y_train)
 
     def predict(self, X):
+        """_summary_ This method predicts the labels for the given data using the trained SVM model.
+        """ 
         return self.model.predict(X)
 
     def evaluate(self, X, y):
+        """This method evaluates the accuracy of the SVM model on the provided dataset."""
         predictions = self.predict(X)
         return accuracy_score(y, predictions)
     
